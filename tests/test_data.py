f43a1e542c879aae2d0698549d0d80bdae088d2c
vyrjana <41105805+vyrjana@users.noreply.github.com>
2022-03-24 16:53:16 +0200

    Added tests for file format parsers

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,10 +4,128 @@
 # the LICENSES folder.
 
 from unittest import TestCase
-from pyimpspec.data.dataset import DataSet, VERSION
-from numpy import array, ndarray, log10 as log
+from pyimpspec import DataSet, parse_data
+from pyimpspec.data.dataset import VERSION
+from numpy import allclose, array, ndarray, log10 as log
 from typing import Callable, Dict, List, Optional
-from os.path import basename, splitext
+from os import walk
+from os.path import basename, dirname, join, splitext
+
+
+def get_control_data() -> DataSet:
+    return DataSet.from_dict(
+        {
+            "label": "Control",
+            "frequency": [
+                10000,
+                7196.85673001152,
+                5179.47467923121,
+                3727.59372031494,
+                2682.69579527973,
+                1930.69772888325,
+                1389.49549437314,
+                1000,
+                719.685673001152,
+                517.947467923121,
+                372.759372031494,
+                268.269579527973,
+                193.069772888325,
+                138.949549437314,
+                100,
+                71.9685673001152,
+                51.7947467923121,
+                37.2759372031494,
+                26.8269579527973,
+                19.3069772888325,
+                13.8949549437314,
+                10,
+                7.19685673001152,
+                5.17947467923121,
+                3.72759372031494,
+                2.68269579527973,
+                1.93069772888325,
+                1.38949549437314,
+                1,
+            ],
+            "real": [
+                109.00918219439,
+                112.057759954682,
+                116.906245842316,
+                124.834566504841,
+                137.770479052544,
+                157.971701100428,
+                186.636916072821,
+                221.690825019137,
+                257.437427532301,
+                288.118363568086,
+                311.563115366785,
+                328.958937177027,
+                342.639052460841,
+                354.649295730333,
+                366.399861210884,
+                378.777952592123,
+                392.326418700802,
+                407.370451984566,
+                424.085899178091,
+                442.527901931489,
+                462.638253653398,
+                484.245515868873,
+                507.068153598917,
+                530.727486829165,
+                554.773334457305,
+                578.720842454073,
+                602.093062736189,
+                624.461682820694,
+                645.478700150494,
+            ],
+            "imaginary": [
+                -26.5556798765152,
+                -35.1662256016599,
+                -46.4663772865298,
+                -60.8522924167586,
+                -78.0893530523511,
+                -96.480585382064,
+                -112.204629862651,
+                -120.39912459346,
+                -118.650600986126,
+                -109.310321223647,
+                -97.2956995983817,
+                -86.5539533982431,
+                -78.8886755242402,
+                -74.587868105523,
+                -73.2559400473505,
+                -74.2956945797458,
+                -77.1022034740841,
+                -81.1148201939911,
+                -85.8172962476514,
+                -90.7274808764653,
+                -95.3931737367316,
+                -99.3992549174071,
+                -102.385330669065,
+                -104.069381709955,
+                -104.270595391674,
+                -102.92415906369,
+                -100.082675458352,
+                -95.9025788682872,
+                -90.618128307383,
+            ],
+        }
+    )
+
+
+def get_test_files(extension: Optional[str] = None) -> List[str]:
+    root: str
+    files: List[str]
+    for root, _, files in walk(dirname(__file__)):
+        break
+    assert len(files) > 0
+    files.sort()
+    files = list(map(lambda _: join(root, _), files))
+    if extension is None:
+        return files
+    files = list(filter(lambda _: splitext(_)[1] == extension, files))
+    assert len(files) > 0
+    return files
 
 
 class TestDataSet(TestCase):
@@ -61,14 +179,16 @@ class TestDataSet(TestCase):
         label: str = "label"
         data = DataSet(freq, Z, mask, path, label)
         self.assertEqual(data.get_label(), label)
-        data = DataSet.from_dict({
-            "frequency": freq,
-            "real": Z.real,
-            "imaginary": Z.imag,
-            "mask": mask,
-            "path": path,
-            "label": label,
-        })
+        data = DataSet.from_dict(
+            {
+                "frequency": freq,
+                "real": Z.real,
+                "imaginary": Z.imag,
+                "mask": mask,
+                "path": path,
+                "label": label,
+            }
+        )
         self.assertEqual(data.get_path(), path)
         self.assertEqual(data.get_label(), label)
         self.assertEqual(data.get_num_points(), 18)
@@ -147,7 +267,9 @@ class TestDataSet(TestCase):
     def test_06_to_dict(self):
         path: str = "/test/path/file.ext"
         label: str = "testing"
-        data: DataSet = DataSet(array([1]), array([complex(1, -1)]), {0: True}, path, label)
+        data: DataSet = DataSet(
+            array([1]), array([complex(1, -1)]), {0: True}, path, label
+        )
         dictionary: dict = data.to_dict()
         self.assertTrue("version" in dictionary, True)
         self.assertTrue("path" in dictionary, True)
@@ -181,4 +303,101 @@ class TestDataSet(TestCase):
         # TODO: Implement
         pass
 
-# TODO: Create tests for the parsers for the various file formats
+
+class TestFormatParsers(TestCase):
+    def validate(self, data: DataSet, control: DataSet):
+        self.assertEqual(control.get_num_points(), data.get_num_points())
+        self.assertTrue(allclose(control.get_frequency(), data.get_frequency()))
+        self.assertTrue(allclose(control.get_impedance(), data.get_impedance()))
+
+    def test_01_csv(self):
+        control: DataSet = get_control_data()
+        path: str
+        for path in get_test_files(".csv"):
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_02_xls(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".xls")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_03_xlsx(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".xlsx")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_04_ods(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".ods")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_05_dta(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".dta")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_06_idf(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".idf")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_07_ids(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files(".ids")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    def test_08_no_extension(self):
+        control: DataSet = get_control_data()
+        paths: List[str] = get_test_files("")
+        self.assertTrue(len(paths) > 0)
+        path: str
+        for path in paths:
+            data: DataSet
+            for data in parse_data(path):
+                self.validate(data, control)
+
+    # TODO: Implement support for .fods files and then implement tests for the parser.
+    """
+    def test_N_fods(self):
+        control: DataSet = get_control_data()
+        f_ctrl = control.get_frequency()
+        Z_ctrl = control.get_impedance()
+        path: str
+        for path in get_test_files(".fods"):
+            for data in parse_data(path):
+                self.assertEqual(control.get_num_points(), data.get_num_points())
+                self.assertTrue(allclose(f_ctrl, data.get_frequency()))
+                self.assertTrue(allclose(Z_ctrl, data.get_impedance()))
+    """
