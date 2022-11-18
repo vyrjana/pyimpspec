fb64a8992cbec1a20da3601f27cb8b5230140d8f
vyrjana <41105805+vyrjana@users.noreply.github.com>
2022-11-18 05:13:15 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -28,16 +28,10 @@ dependencies = [
     "tabulate>=0.8.10",  # Required by pandas to generate Markdown tables.
 ]
 
-with open("requirements.txt", "w") as fp:
-    fp.write("\n".join(dependencies))
-
 dev_dependencies = [
     "flake8",
     "setuptools",
 ]
-
-with open("dev-requirements.txt", "w") as fp:
-    fp.write("\n".join(dev_dependencies))
 
 optional_dependencies = {
     "cvxpy": "cvxpy>=1.2.1",  # Used in the DRT calculations (TR-RBF method)
@@ -51,39 +45,49 @@ data_files = [
     "LICENSES/README.md",
 ] + licenses
 
-setup(
-    name="pyimpspec",
-    version="3.2.0",
-    author="pyimpspec developers",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    data_files=data_files,
-    url="https://vyrjana.github.io/pyimpspec",
-    project_urls={
-        "Documentation": "https://vyrjana.github.io/pyimpspec/api/",
-        "Source Code": "https://github.com/vyrjana/pyimpspec",
-        "Bug Tracker": "https://github.com/vyrjana/pyimpspec/issues",
-    },
-    license="GPLv3",
-    description="A package for parsing, validating, analyzing, and simulating impedance spectra.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    install_requires=dependencies,
-    extras_require=optional_dependencies,
-    python_requires=">=3.8",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: MacOS",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Scientific/Engineering :: Chemistry",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering",
-    ],
-)
+version = "3.2.0"
+
+if __name__ == "__main__":
+    with open("requirements.txt", "w") as fp:
+        fp.write("\n".join(dependencies))
+    with open("dev-requirements.txt", "w") as fp:
+        fp.write("\n".join(dev_dependencies))
+    with open("version.txt", "w") as fp:
+        fp.write(version)
+    assert version.strip != ""
+    setup(
+        name="pyimpspec",
+        version=version,
+        author="pyimpspec developers",
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        include_package_data=True,
+        data_files=data_files,
+        url="https://vyrjana.github.io/pyimpspec",
+        project_urls={
+            "Documentation": "https://vyrjana.github.io/pyimpspec/api/",
+            "Source Code": "https://github.com/vyrjana/pyimpspec",
+            "Bug Tracker": "https://github.com/vyrjana/pyimpspec/issues",
+        },
+        license="GPLv3",
+        description="A package for parsing, validating, analyzing, and simulating impedance spectra.",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        install_requires=dependencies,
+        extras_require=optional_dependencies,
+        python_requires=">=3.8",
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Scientific/Engineering :: Chemistry",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Topic :: Scientific/Engineering",
+        ],
+    )
